ed54efc2c8bba01e1623c49f7d4a49fe26a45dc7
Einzberg <maxfan159@gmail.com>
2017-06-24 16:05:53 -0700

    stack urls

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -5,15 +5,15 @@ class QuotesSpider(scrapy.Spider):
     name = "quotes"
 
     def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
+        urls = []
+        for x in range(50):
+            urls.append("http://www.rinkworks.com/jokes/jokes" + str(x) + ".shtml")
+
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
+        page = response.url.split("/")[-1]
         filename = 'quotes-%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
