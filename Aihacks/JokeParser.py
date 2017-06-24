46c680eb04871c6ec6c08032ab6aafab25b2504c
Einzberg <maxfan159@gmail.com>
2017-06-24 16:44:54 -0700

    parser

--- Aihacks/JokeParser.py
+++ Aihacks/JokeParser.py
@@ -1,10 +1,21 @@
 from bs4 import BeautifulSoup
 import urllib.request as urllib2
+import re
+import csv
 
-url = "http://www.pythonforbeginners.com"
+urls = []
+for x in range(1, 2):
+    urls.append("http://www.rinkworks.com/jokes/jokes" + str(x) + ".shtml")
 
-content = urllib2.urlopen(url).read()
+for url in urls:
+    content = urllib2.urlopen(url).read()
+    soup = BeautifulSoup(content, "lxml")
+    souptext = soup.get_text()
+    asdf = re.split('#\d', souptext)
 
-soup = BeautifulSoup(content)
-
-print(soup.prettify())
+    with open('eggs.csv', 'wb') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=' ',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for idx, jokes in enumerate(asdf):
+            if idx == 0 or idx == len(asdf) -1: continue
+            print(jokes)
