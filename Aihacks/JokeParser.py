69c4657ddcac383b6c9bd83f83e6e75a4d4d22e2
monaz32 <mac@macmatoMacBook-Air.local>
2017-06-24 18:53:45 -0700

    remove comments

--- Aihacks/JokeParser.py
+++ Aihacks/JokeParser.py
@@ -13,18 +13,6 @@ for n in range(1,90):
 
     blockList = soup.find("div", {"class" : "content"}).find_all("ul")
 
-    # for block in blockList:
-    #     if len(str(block).split("\n")) == 4:
-    #         count = 0
-    #         modLine = ""
-    #         for line in str(block).split("\n"):
-    #             count = count + 1
-    #             if (count == 2):
-    #                 modLine =  modLine + line [4:-5]
-    #             elif (count == 3):
-    #                 modLine = modLine + "." + line[4:-5]
-    #         data.append([modLine])
-
     for block in blockList:
         if len(str(block).split("\n")) == 4:
             matchObj = re.match(r"<ul>\n<li>(.*)</li>\n<li>(.*)</li>\n</ul>", str(block))
