dafefb535d0581a588d6ee098ae4c512eca5f0b3
monaz32 <mac@macmatoMacBook-Air.local>
2017-06-24 17:58:21 -0700

    save all jokes

--- Aihacks/JokeParser.py
+++ Aihacks/JokeParser.py
@@ -3,6 +3,7 @@ import urllib.request as urllib2
 import re
 import csv
 
+<<<<<<< Updated upstream
 urls = []
 for x in range(1, 5):
     urls.append("http://www.rinkworks.com/jokes/jokes" + str(x) + ".shtml")
@@ -19,4 +20,29 @@ for url in urls:
         for idx, jokes in enumerate(asdf):
             if idx == 0 or idx == len(asdf) -1: continue
             spamwriter.writerow([jokes, "true"])
+=======
+for n in range(1,2):
+
+    url = "http://www.rinkworks.com/jokes/jokes" + str(n) + ".shtml"
+
+    content = urllib2.urlopen(url).read()
+
+    soup = BeautifulSoup(content, "lxml")
+
+    blockList = soup.find("div", {"class" : "content"}).find_all("ul")
+
+    data = []
+    for block in blockList:
+        if len(str(block).split("\n")) == 4:
+            count = 0
+            modLine = ""
+            for line in str(block).split("\n"):
+                count = count + 1
+                if (count == 2):
+                    modLine =  modLine + line [4:-5]
+                elif (count == 3):
+                    modLine = modLine + "." + line[4:-5]
+            data.append([modLine])
+
+>>>>>>> Stashed changes
 
