5c2b0eb9b9d88f68cf50e0c496e2f3b254c6af8e
monaz32 <mac@macmatoMacBook-Air.local>
2017-06-24 18:52:37 -0700

    save all jokes

--- Aihacks/JokeParser.py
+++ Aihacks/JokeParser.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
-import urllib.request as urllib2
+import urllib2
+import re
 
-for n in range(1,2):
+data = []
+for n in range(1,90):
 
     url = "http://www.rinkworks.com/jokes/jokes" + str(n) + ".shtml"
 
@@ -11,17 +13,21 @@ for n in range(1,2):
 
     blockList = soup.find("div", {"class" : "content"}).find_all("ul")
 
-    data = []
+    # for block in blockList:
+    #     if len(str(block).split("\n")) == 4:
+    #         count = 0
+    #         modLine = ""
+    #         for line in str(block).split("\n"):
+    #             count = count + 1
+    #             if (count == 2):
+    #                 modLine =  modLine + line [4:-5]
+    #             elif (count == 3):
+    #                 modLine = modLine + "." + line[4:-5]
+    #         data.append([modLine])
+
     for block in blockList:
         if len(str(block).split("\n")) == 4:
-            count = 0
-            modLine = ""
-            for line in str(block).split("\n"):
-                count = count + 1
-                if (count == 2):
-                    modLine =  modLine + line [4:-5]
-                elif (count == 3):
-                    modLine = modLine + "." + line[4:-5]
-            data.append([modLine])
-
-
+            matchObj = re.match(r"<ul>\n<li>(.*)</li>\n<li>(.*)</li>\n</ul>", str(block))
+            if matchObj:
+                line = str(matchObj.group(1)) + "." + str(matchObj.group(2))
+            data.append([line])
